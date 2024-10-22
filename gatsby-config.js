cc227a50cf4ca6c9ee688c9dd34f9e7ecb25b765
daussysimon <daussy.simon@gmail.com>
2024-10-22 12:33:49 -0700

    correction gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,24 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
-
 const siteUrl = process.env.URL || `https://fallback.net`;
+
 module.exports = {
   flags: {
     DEV_SSR: false,
   },
+  siteMetadata: {
+    // If you didn't use the resolveSiteUrl option this needs to be set
+    siteUrl: siteUrl,
+  },
   plugins: [
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
-    (module.exports = {
-      siteMetadata: {
-        // If you didn't use the resolveSiteUrl option this needs to be set
-        siteUrl: siteUrl,
-      },
-      plugins: [`gatsby-plugin-sitemap`],
-    }),
     "gatsby-plugin-fontawesome",
     "gatsby-plugin-mdx",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
-
+    "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-source-filesystem",
       options: {
@@ -66,54 +63,5 @@ module.exports = {
         ],
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-feed`,
-    //   options: {
-    //     query: `
-    //       {
-    //         site {
-    //           siteMetadata {
-    //             title
-    //             description
-    //             siteUrl
-    //             site_url: siteUrl
-    //           }
-    //         }
-    //       }
-    //     `,
-    //     feeds: [
-    //       {
-    //         serialize: ({ query: { site, allMarkdownRemark } }) => {
-    //           return allMarkdownRemark.nodes.map((node) => {
-    //             return Object.assign({}, node.frontmatter, {
-    //               description: node.excerpt,
-    //               date: node.frontmatter.date,
-    //               url: site.siteMetadata.siteUrl + node.fields.slug,
-    //               guid: site.siteMetadata.siteUrl + node.fields.slug,
-    //               custom_elements: [{ "content:encoded": node.html }],
-    //             });
-    //           });
-    //         },
-    //         query: `{
-    //           allMarkdownRemark(sort: {frontmatter: {date: DESC}}) {
-    //             nodes {
-    //               excerpt
-    //               html
-    //               fields {
-    //                 slug
-    //               }
-    //               frontmatter {
-    //                 title
-    //                 date
-    //               }
-    //             }
-    //           }
-    //         }`,
-    //         output: "/rss.xml",
-    //         title: "Gatsby Starter Blog RSS Feed",
-    //       },
-    //     ],
-    //   },
-    // },
   ],
 };
