0764fa07f610f67e19e7beaeae8840e527a743fb
daussysimon <daussy.simon@gmail.com>
2024-10-23 09:34:43 -0700

    test

--- src/templates/index-page.jsx
+++ src/templates/index-page.jsx
@@ -108,141 +108,135 @@ function IndexPage({ data }) {
         </div>
       </header>
 
-      <main className="main">
-        <ParallaxBanner
-          layers={[
-            {
-              image: `https://sejaluzbeaute.netlify.app/img/mainBackground-large.webp`,
-              speed: -55,
-            },
-          ]}
-        >
-          <div className="main-container">
-            <div className="main-parallax">
-              <section className="main-description">
-                <div className="main-description-container">
-                  <h2>{pageData.descriptionSection.title}</h2>
-                  <p className="main-description-container-text">
-                    {pageData.descriptionSection.text}
-                  </p>
-                </div>
-                <GatsbyImage
-                  className="main-description-container-image"
-                  image={descriptionImage}
-                  alt="personne entrain de réaliser un massage"
-                />
-              </section>
-              <section className="main-massages" id="massage">
-                <h2 className="main-massages-title">
-                  {pageData.massage.title}
-                </h2>
-                <ul className="main-massages-list">
-                  {massagesData.map((item, key) => (
-                    <li className="card" key={key}>
-                      <Card data={item} />
-                    </li>
-                  ))}
-                </ul>
-                <p className="main-massages-asterisque">
-                  {pageData.massage.asterisque}
+      <main
+        className="main"
+        style={{
+          background: `url(/img/mainBackground-large.webp)`,
+        }}
+      >
+        <div className="main-container">
+          <div className="main-parallax">
+            <section className="main-description">
+              <div className="main-description-container">
+                <h2>{pageData.descriptionSection.title}</h2>
+                <p className="main-description-container-text">
+                  {pageData.descriptionSection.text}
                 </p>
-              </section>
-              <section className="main-contact" id="contact">
-                <div className="main-contact-container">
-                  <GatsbyImage
-                    className="main-contact-image"
-                    image={contactImage}
-                    alt="photo représentant un salon de massage fleuri"
-                  />
-                  <div className="main-contact-details">
-                    <h2 className="main-contact-title">Contact</h2>
+              </div>
+              <GatsbyImage
+                className="main-description-container-image"
+                image={descriptionImage}
+                alt="personne entrain de réaliser un massage"
+              />
+            </section>
+            <section className="main-massages" id="massage">
+              <h2 className="main-massages-title">{pageData.massage.title}</h2>
+              <ul className="main-massages-list">
+                {massagesData.map((item, key) => (
+                  <li className="card" key={key}>
+                    <Card data={item} />
+                  </li>
+                ))}
+              </ul>
+              <p className="main-massages-asterisque">
+                {pageData.massage.asterisque}
+              </p>
+            </section>
+            <section className="main-contact" id="contact">
+              <div className="main-contact-container">
+                <GatsbyImage
+                  className="main-contact-image"
+                  image={contactImage}
+                  alt="photo représentant un salon de massage fleuri"
+                />
+                <div className="main-contact-details">
+                  <h2 className="main-contact-title">Contact</h2>
 
-                    <p className="main-contact-details-item">
-                      <FontAwesomeIcon
-                        className="main-contact-details-item-icon"
-                        icon={faHouse}
-                      />{" "}
-                      <span className="main-contact-details-item-value">
-                        <span>A domicile: </span>
-                        <span className="main-contact-details-item-value-dat">
-                          {pageData.contact.adresse}
-                        </span>
+                  <p className="main-contact-details-item">
+                    <FontAwesomeIcon
+                      className="main-contact-details-item-icon"
+                      icon={faHouse}
+                    />{" "}
+                    <span className="main-contact-details-item-value">
+                      <span>A domicile: </span>
+                      <span className="main-contact-details-item-value-dat">
+                        {pageData.contact.adresse}
                       </span>
-                    </p>
+                    </span>
+                  </p>
 
-                    <a
-                      className="main-contact-details-item"
-                      href={`mailto:${pageData.contact.email}?subject:Prise de contact`}
-                    >
+                  <a
+                    className="main-contact-details-item"
+                    href={`mailto:${pageData.contact.email}?subject:Prise de contact`}
+                  >
+                    <FontAwesomeIcon
+                      className="main-contact-details-item-icon"
+                      icon={faEnvelope}
+                    />
+
+                    <span className="main-contact-details-item-value">
+                      <span>Adresse email: </span>
+                      <span className="main-contact-details-item-value-dat">
+                        {" "}
+                        {pageData.contact.email}
+                      </span>
+                    </span>
+                  </a>
+                  <a
+                    className="main-contact-details-item"
+                    href={`tel:${pageData.contact.phone}`}
+                  >
+                    <span>
                       <FontAwesomeIcon
                         className="main-contact-details-item-icon"
-                        icon={faEnvelope}
+                        icon={faPhone}
                       />
+                    </span>
 
-                      <span className="main-contact-details-item-value">
-                        <span>Adresse email: </span>
-                        <span className="main-contact-details-item-value-dat">
-                          {" "}
-                          {pageData.contact.email}
-                        </span>
+                    <span className="main-contact-details-item-value">
+                      <span>Numéro de téléphone: </span>
+                      <span className="main-contact-details-item-value-dat">
+                        {pageData.contact.phone}
                       </span>
-                    </a>
-                    <a
-                      className="main-contact-details-item"
-                      href={`tel:${pageData.contact.phone}`}
-                    >
-                      <span>
-                        <FontAwesomeIcon
-                          className="main-contact-details-item-icon"
-                          icon={faPhone}
+                    </span>
+                  </a>
+                  <div className="main-contact-details-socialMedia">
+                    {pageData.contact.instagram.visible && (
+                      <a
+                        className="main-contact-details-item"
+                        href={
+                          isMobile
+                            ? `instagram://user?username=${pageData.contact.instagram.link}`
+                            : `https://www.instagram.com/${pageData.contact.instagram.link}`
+                        }
+                      >
+                        <StaticImage
+                          src="../assets/images/instagram.png"
+                          alt="logo instagram"
                         />
-                      </span>
-
-                      <span className="main-contact-details-item-value">
-                        <span>Numéro de téléphone: </span>
-                        <span className="main-contact-details-item-value-dat">
-                          {pageData.contact.phone}
-                        </span>
-                      </span>
-                    </a>
-                    <div className="main-contact-details-socialMedia">
-                      {pageData.contact.instagram.visible && (
-                        <a
-                          className="main-contact-details-item"
-                          href={
-                            isMobile
-                              ? `instagram://user?username=${pageData.contact.instagram.link}`
-                              : `https://www.instagram.com/${pageData.contact.instagram.link}`
-                          }
-                        >
-                          <StaticImage
-                            src="../assets/images/instagram.png"
-                            alt="logo instagram"
-                          />
-                        </a>
-                      )}
-                      {pageData.contact.facebook.visible && (
-                        <a
-                          className="main-contact-details-item"
-                          href={
-                            isMobile
-                              ? `fb://profile/${pageData.contact.facebook.link}`
-                              : `https://www.facebook.com/${pageData.contact.facebook.link}`
-                          }
-                        >
-                          <StaticImage
-                            src="../assets/images/facebook.png"
-                            alt="logo facebook"
-                          />
-                        </a>
-                      )}
-                    </div>
+                      </a>
+                    )}
+                    {pageData.contact.facebook.visible && (
+                      <a
+                        className="main-contact-details-item"
+                        href={
+                          isMobile
+                            ? `fb://profile/${pageData.contact.facebook.link}`
+                            : `https://www.facebook.com/${pageData.contact.facebook.link}`
+                        }
+                      >
+                        <StaticImage
+                          src="../assets/images/facebook.png"
+                          alt="logo facebook"
+                        />
+                      </a>
+                    )}
                   </div>
                 </div>
-              </section>
-            </div>
+              </div>
+            </section>
           </div>
-        </ParallaxBanner>
+        </div>
       </main>
       <footer className="footer">
         <p>
